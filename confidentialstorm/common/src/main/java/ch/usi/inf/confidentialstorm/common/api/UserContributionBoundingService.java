a3aac0d0bc988589a771e0cfd057c10be6e04fc4
Luca Di Bello <lucadibello@proton.me>
2025-12-03 15:31:29 +0000

    fix: now added @EnclaveService annotation to UserContributionBoundingService

--- confidentialstorm/common/src/main/java/ch/usi/inf/confidentialstorm/common/api/UserContributionBoundingService.java
+++ confidentialstorm/common/src/main/java/ch/usi/inf/confidentialstorm/common/api/UserContributionBoundingService.java
@@ -3,7 +3,9 @@ package ch.usi.inf.confidentialstorm.common.api;
 import ch.usi.inf.confidentialstorm.common.api.model.UserContributionBoundingRequest;
 import ch.usi.inf.confidentialstorm.common.api.model.UserContributionBoundingResponse;
 import ch.usi.inf.confidentialstorm.common.crypto.exception.*;
+import org.apache.teaclave.javasdk.common.annotations.EnclaveService;
 
+@EnclaveService
 public interface UserContributionBoundingService {
     UserContributionBoundingResponse check(UserContributionBoundingRequest request) throws EnclaveServiceException;
 }
